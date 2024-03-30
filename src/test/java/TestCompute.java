0c22755b45cf4f12d9fa695c1878591ea074035b
omerilbilgi <omerilbilgi4@gmail.com>
2024-03-30 11:05:38 +0300

    TestCompute.java was updated

--- src/test/java/TestCompute.java
+++ src/test/java/TestCompute.java
@@ -1,17 +1,50 @@
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCompute {
-  Compute c;
+    private Compute compute;
+    private MessageQueue mq;
 
-  @Test
-  public void example() {
-    MessageQueue mq = mock(MessageQueue.class);
-    c = new Compute(mq);
-    assertTrue(true);
-  }
+    @Before
+    public void setUp() {
+        mq = mock(MessageQueue.class);
+        compute = new Compute(mq);
+    }
+
+    @Test
+    public void testCountNumberOfOccurrences_QueueIsEmpty() {
+        when(mq.size()).thenReturn(0);
+        assertEquals(-1, compute.countNumberOfOccurrences("element"));
+    }
+
+    @Test
+    public void testCountNumberOfOccurrences_ElementNotPresent() {
+        when(mq.size()).thenReturn(2);
+        when(mq.contains(anyString())).thenReturn(false);
+        assertEquals(0, compute.countNumberOfOccurrences("element"));
+    }
+
+    @Test
+    public void testCountNumberOfOccurrences_ElementPresentOnce() {
+        when(mq.size()).thenReturn(3);
+        when(mq.contains("element")).thenReturn(true);
+        when(mq.getAt(0)).thenReturn("element");
+        when(mq.getAt(1)).thenReturn("anotherElement");
+        when(mq.getAt(2)).thenReturn("yetAnotherElement");
+        assertEquals(1, compute.countNumberOfOccurrences("element"));
+    }
+
+    @Test
+    public void testCountNumberOfOccurrences_ElementPresentMultipleTimes() {
+        when(mq.size()).thenReturn(4);
+        when(mq.contains("element")).thenReturn(true);
+        when(mq.getAt(0)).thenReturn("element");
+        when(mq.getAt(1)).thenReturn("anotherElement");
+        when(mq.getAt(2)).thenReturn("element");
+        when(mq.getAt(3)).thenReturn("yetAnotherElement");
+        assertEquals(2, compute.countNumberOfOccurrences("element"));
+    }
 }
